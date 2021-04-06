ad37a402636a0d11795d5e02677552dd3b0c3754
Edina Mucsi <edina.mucsi9@gmail.com>
2021-04-06 20:13:19 +0200

    pmi and matrix factorization

--- FRICI/AI/adatbeolvasás.py
+++ FRICI/AI/adatbeolvasás.py
@@ -9,8 +9,7 @@ df.ingredients = df.ingredients.apply(lambda ings : [ing.lower() for ing in ings
 #print(df.head(10))
 #print(df.columns)
 import itertools
-# Example of what the itertools.combinations function does.
-print(list(itertools.combinations(df.ingredients[0][:5], 2)))
+
 
 from collections import Counter
 cooc_counts = Counter()
@@ -25,6 +24,50 @@ for ingredients in df.ingredients:
         cooc_counts[(ing_a, ing_b)] += 1
 
 cooc_df = pd.DataFrame(((ing_a, ing_b, ing_count[ing_a], ing_count[ing_b], cooc) for (ing_a, ing_b), cooc in cooc_counts.items()), columns=['a', 'b', 'a_count', 'b_count', 'cooc'])
-print(cooc_df.sample(10))
+#print(cooc_df.sample(10))
 
-print(cooc_df[cooc_df.a == 'chillies'].sort_values('cooc', ascending=False).head(10))
+#print(cooc_df[cooc_df.a == 'chillies'].sort_values('cooc', ascending=False).head(10))
+
+p_a = cooc_df.a_count / sum(ing_count.values())
+p_b = cooc_df.b_count / len(ing_count.values())
+p_a_b = cooc_df.cooc / cooc_df.cooc.sum()
+cooc_df['pmi'] = np.log(p_a_b / (p_a * p_b))
+
+#matrix factorization part
+
+
+
+from scipy.sparse import csr_matrix
+data_df = cooc_df[cooc_df.pmi > 0].copy()
+# Since the matrix is symetric, we add the same values for (b,a) as we have for (a,b)
+data_df_t = data_df.copy()
+data_df.a, data_df.b = data_df.b, data_df.a
+data_df = pd.concat([data_df, data_df_t])
+
+rows_idx, row_keys = pd.factorize(data_df.a)
+cols_idx, col_keys = pd.factorize(data_df.b)
+values = data_df.pmi
+
+matrix = csr_matrix((values, (rows_idx, cols_idx)))
+key_to_row = {key: idx for idx, key in enumerate(row_keys)}
+
+from sklearn.decomposition import TruncatedSVD
+
+svd = TruncatedSVD(200)
+factors = svd.fit_transform(matrix)
+
+from sklearn.metrics.pairwise import cosine_similarity
+def most_similar(ingredient, topn=10):
+    if ingredient not in key_to_row:
+        print("Unknown ingredient.")
+    factor = factors[key_to_row[ingredient]]
+    cosines = cosine_similarity([factor], factors)[0]
+    indices = cosines.argsort()[::-1][:topn + 1]
+    keys = [row_keys[idx] for idx in indices if idx != key_to_row[ingredient]]
+    return keys, cosines[indices]
+
+def display_most_similar(ingredient, topn=10):
+    print("- Most similar to '{}'".format(ingredient))
+    for similar_ing, score in zip(*most_similar(ingredient, topn)):
+        print("  . {} : {:.2f}".format(similar_ing, score))
+display_most_similar('chile powder')
