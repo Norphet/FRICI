4ef49609b1807d69eee9306c4e09af5f27680948
Edina Mucsi <edina.mucsi9@gmail.com>
2021-04-23 18:20:17 +0200

    keresés

--- FRICI/DataManagement/DataUpload.py
+++ FRICI/DataManagement/DataUpload.py
@@ -1,12 +1,35 @@
 import pandas as pd
 import pickle
+matrix = pickle.load(open("data.bin", "rb"))
 
-rawdatabase = pd.read_excel(r'E:\Internet\Downloads\frici_data.xlsx')
-print(rawdatabase)
-print(rawdatabase.dtypes)
+def beolvas(adatbazis):
+    ingredient=[]
+    quantity=[]
+    receptek=[]
+    key= input("Do you want to give more ingredients, y for yes n for no")
+    szamlalo = 0
+    while key=='y':
+        ingredient.append(input("Type in the ingredient that you have"))
+        quantity.append(input("Type in the quantity that you have"))
+        counter1=0
+        counter2=0
 
-pickle.dump(rawdatabase, open("data.bin", "wb"))
+        for i in range(adatbazis.shape[1]):
+            if adatbazis.columns[i] == ingredient[szamlalo]:
+                counter1=counter1+1
 
-# How to Import data:
-# import pickle
-# matrix = pickle.load(open("data.bin", "rb"))
+        key = input("Do you want to give more ingredients, y for yes n for no")
+        if key=='y':
+            szamlalo = szamlalo + 1
+    for i in range(adatbazis.shape[0]):
+            if (adatbazis[ingredient[szamlalo]][i] !=0) & (adatbazis[ingredient[szamlalo-1]][i] !=0):
+                receptek.append(adatbazis['name'][i])
+    return receptek
+
+
+
+matrix = pickle.load(open("data.bin", "rb"))
+print(matrix)
+print(beolvas(matrix))
+
+
